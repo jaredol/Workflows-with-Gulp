68cd8d1542f521ea3dea7dc786185bceef1dfa8f
Jared O <jared.oloughlin@gmail.com>
2015-07-16 16:13:33 +1000

    include GIT info in package file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,27 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util')
+var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
 
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
+
+
+gulp.task('coffee', function(){
+  gulp.src('components/coffee/tagline.coffee')
+    .pipe(coffee({ bare: true})
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
+});
 
 gulp.task('log', function(){
   gutil.log("Workflows are awesome");
 });
+
+
+//gulp.task('')
