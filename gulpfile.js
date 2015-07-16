88389f54882b93808feafae0e8485164a4907ccf
Jared O <jared.oloughlin@gmail.com>
2015-07-16 16:46:23 +1000

    Add Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -19,8 +20,12 @@ gulp.task('coffee', function(){
     .pipe(gulp.dest('components/scripts'))
 });
 
-gulp.task('log', function(){
-  gutil.log("Workflows are awesome");
+
+gulp.task('js', function(){
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest('builds/development/js'))
 });
 
 
